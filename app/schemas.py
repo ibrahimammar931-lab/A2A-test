6145598bf00a231887a56ff134a6e02b958571b2
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-07-19 12:39:36 +0100

    KAN-17 Add Roles

--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,14 @@
 from pydantic import BaseModel
-
+from .enums import Role
 
 class UserCreate(BaseModel):
     name: str
     email: str
-
+    role: Role = Role.developer
 
 class TaskCreate(BaseModel):
     title: str
     description: str
-
 
 class TaskUpdate(BaseModel):
     title: str | None = None
