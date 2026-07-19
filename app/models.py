d5fa25a7fdcf22bcceb01ab23e00bffd3d3624ef
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-07-19 12:39:34 +0100

    KAN-17 Add Roles

--- app/models.py
+++ app/models.py
@@ -1,8 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-
 from .database import Base
-
+from .enums import Role
 
 class User(Base):
     __tablename__ = "users"
@@ -10,9 +9,9 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     email = Column(String, unique=True)
+    role = Column(String, default=Role.developer)
 
     tasks = relationship("Task", back_populates="user")
-
 
 class Task(Base):
     __tablename__ = "tasks"
