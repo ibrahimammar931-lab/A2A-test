dffd0da7a9912c2fb0378fc6424f79c37eabbc10
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-07-19 12:32:48 +0100

    KAN-16 Add JWT Authentication

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,17 +1,21 @@
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
-
 from ..database import SessionLocal
 from .. import crud, schemas
+from ..auth import oauth2_scheme, verify_token
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-
 @router.post("/")
-def create(user: schemas.UserCreate):
+def create(user: schemas.UserCreate, token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = verify_token(token, credentials_exception)
     db: Session = SessionLocal()
     return crud.create_user(db, user)
-
 
 @router.get("/")
 def list_users():
