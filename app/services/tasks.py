8d55d0b25fc77e7184d178638b3b90db027db936
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-07-20 10:16:17 +0100

    KAN-13 Add Due Date and Overdue Endpoint

--- app/services/tasks.py
+++ app/services/tasks.py
@@ -1,11 +1,12 @@
 from .. import crud, schemas
 from ..database import SessionLocal
+from datetime import datetime
 
 def create_task(task: schemas.TaskCreate):
     db: Session = SessionLocal()
     return crud.create_task(db, task)
 
-def get_tasks():
+def get_tasks):
     db: Session = SessionLocal()
     return crud.get_tasks(db)
 
@@ -22,3 +23,8 @@ def delete_task(task_id: int):
     if not deleted:
         raise HTTPException(404, "Task not found")
     return {"message": "Deleted"}
+
+def get_overdue_tasks():
+    db: Session = SessionLocal()
+    overdue_tasks = db.query(Task).filter(Task.due_date < datetime.now()).all()
+    return overdue_tasks
